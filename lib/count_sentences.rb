baa1897a8dacd2b61a1af0ab0f613ab1a2d48b9d
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-25 17:18:50 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,36 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self[-1] == "."
+      true 
+    else 
+      false 
+    end
   end
 
   def question?
-
+    if self[-1] == "?"
+      true 
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self[-1] == "!"
+      true 
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    sentence_count = 0
+    self.split.each do |word|
+      if word[-1] == "." || word[-1] == "?" || word[-1] == "!" 
+        sentence_count+=1
+      end
+    end
+    sentence_count
   end
 end
